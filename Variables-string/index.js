56688eedb62a9f7d6e7c304e7d1f77ae6f208493
Edaly09 <edalyminthe@gmail.com>
2022-06-20 14:18:54 +0200

    exercises

--- Variables-string/index.js
+++ Variables-string/index.js
@@ -12,3 +12,48 @@
 
 // const name = `Edaly`
 // console.log(`Nice to meet you  ${name}`)
+
+// les exos
+// ## 04 - String Length
+
+// const sentence = `"I'm a new text !"`
+// console.log(sentence.length)
+
+// ## 05 - Replace
+// const food = "croissant is meh"
+// food.replace("meh", "so good")
+// console.log(food)
+
+
+// <aside>
+// 💡 À partir de maintenant, on vous donnera un peu moins d'indices dans les exercices. C'est à vous de trouver toutes les étapes nécessaires, on ne vous indiquera que les consignes de bases !
+
+// </aside>
+
+// ## 06 - Up and Down
+// const basic = "This is cool"
+// const basicUp = "This is cool"
+// const basicDown = "This is cool"
+// const basicUp.toUpperCasse
+// const basicDown.tolowercasse
+// console.log(basic)
+// console.log(basicUp)
+// console.log(basicDown)
+
+
+// ## 07 - Split
+// const word = "banana"
+// const letters = word.split("")
+	
+// console.log(letters)
+    
+
+
+
+
+
+
+// ## 08 - Template
+// const age = 24
+// const template = "I' m ____ years old"
+// console.log(`I' m, ${age}`)
