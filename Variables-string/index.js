2791ca191a4bb57bea2e6da2b9b20d9f45b15bb0
Edaly09 <edalyminthe@gmail.com>
2022-06-20 13:06:40 +0200

    prise en main

---
+++ Variables-string/index.js
@@ -0,0 +1,14 @@
+// test
+
+// console.log("Hello world")
+
+// ## 02 - String
+
+
+// const sentence = "My name is Edaly"
+// console.log(sentence)
+
+// "## 03 - Concaténation
+
+// const name = `Edaly`
+// console.log(`Nice to meet you  ${name}`)
